a6ebe633b378acedc63f4464ee96e7496d5d64f4
yinka3 <adedewe.a@northeastern.edu>
2025-03-12 10:41:52 -0400

    added pictures and info

--- backend/blackbutler.py
+++ backend/blackbutler.py
@@ -21,10 +21,13 @@ app.add_middleware(
 model = genai.Client(api_key=os.getenv('GEMINI_KEY'))
 
 prompt_context = """You are an AI Assistant called Black Butler. Your goal is to provide
-accurate information to users based on the engineers they are asking about. If possible give
-them additional links about the engineer for them to learn more about them. You are to speak to
-users in ebonics and professional.
+accurate information to users based on the engineers they are asking about. If domain knowledge
+is not sufficient, web search information based on user's prompt. If possible give
+them additional links about the engineer for them to learn more about them.
 """
+
+def lowercase(words):
+    return ''.join(word.lower() for word in words)
 
 
 @app.post("/query_model")
@@ -44,8 +47,17 @@ async def query(request: Request):
         contents=text_prompt,
         config=types.GenerateContentConfig(
             system_instruction=prompt_context,
-            tools=[types.Tool(google_search=types.GoogleSearch())],
-            temperature=0.2
+            tools=[
+                types.Tool(google_search=types.GoogleSearch())],
+            temperature=0.2,
+            top_p=1.5,
+            max_output_tokens=100,
+            stop_sequences=["Have a good day", "Bye", "Thank you bye"],
+            safety_settings= [
+                types.SafetySetting(
+                    category='HARM_CATEGORY_HATE_SPEECH',
+                    threshold='BLOCK_MEDIUM_AND_ABOVE'),
+            ]
         )
     )
 
